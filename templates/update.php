a351141a49a50743feb6d1c220471e830e9560b6
Ke Zhang <57934117+keke980418@users.noreply.github.com>
2021-03-08 18:44:19 -0500

    Update update.php

--- templates/update.php
+++ templates/update.php
@@ -1,13 +1,74 @@
 <!DOCTYPE html>
-<?php require_once('../db/dbc.php'); ?>
+<?php
+require_once('../db/dbc.php');
+if(!isset($_SESSION))
+{
+    session_start();
+}  ?>
 <html>
   <head>
     <meta charset="utf-8">
     <title>Update</title>
     <link rel="stylesheet" type="text/css" href="../css/styles.css"/>
+    <script>
+      function search() {
+        var search = document.getElementById("search");
+        if (search.style.display=="none") {
+          search.style.display="block";
+        }
+        else{
+          search.style.display="none";
+        }
+      }
+    </script>
   </head>
 
   <body>
+    <ul id = "menu">
+      <li><a href="../index.php">Home</a></li>
+      <li><a href="#">db Maintain</a>
+        <ul>
+          <li><a href="insert.php">Insert</a></li>
+          <li><a href="delete.php">Delete</a></li>
+          <li><a href="select.php">Select</a></li>
+          <li><a href="update.php">Update</a></li>
+        </ul>
+      </li>
+      <li><a href="aboutus.php">About Us</a></li>
+      <li><a href="contactus.php">Contact Us</a></li>
+
+      <?php if (isset($_SESSION['user'])){
+        echo "<li style='float:right'><a href='../scripts/logout.php'>Sign Out</a></li>";
+        echo "<li style='float:right'><a href=''>Welcome ". $_SESSION['user'] ."</a></li>";
+        echo '<li style="float:right"><a href="#"><span onclick="search()">Search</span></a></li>';
+      }
+      else {
+        echo "<li style='float:right'><a href='signup.php'>Sign Up</a></li>";
+      }
+      ?>
+
+      <li><a href="reviews.php">Reviews</a></li>
+      <?php if (!isset($_SESSION['user'])){
+        echo "<li style='float:right'><a href='login.php'>Login</a></li>";
+      }
+      ?>
+      <!-- <li style="float:right"><a href="#"><span onclick="search()">Search</span></a></li> -->
+      <li><a href="#">Type of Services</a>
+        <ul>
+          <li><a href="rideshare.php">Rideshare</a></li>
+          <li><a href="ride_and_delivery.php">Ride & Delivery</a></li>
+        </ul>
+      </li>
+
+    </ul>
+
+    <div id="search" style="float:right; display:none;">
+      <form action="#" method="POST">
+        <input type="text" placeholder="Search..." name="search">
+        <button name="submit_result" type="submit">submit</button>
+      </form>
+    </div>
+    
     <h2>Update</h2>
     <p style="width:auto;">Please enter information to upadte tables:</p>
 
