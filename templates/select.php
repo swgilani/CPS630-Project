b86a9b3f4db90008ac8d9117705199069a3937d3
Ke Zhang <57934117+keke980418@users.noreply.github.com>
2021-03-08 18:36:58 -0500

    Update select.php

--- templates/select.php
+++ templates/select.php
@@ -1,46 +1,65 @@
 <!DOCTYPE html>
-<?php require_once('../db/dbc.php'); ?>
+<?php
+require_once('../db/dbc.php');
+if(!isset($_SESSION))
+{
+    session_start();
+}  ?>
 <html>
   <head>
     <meta charset="utf-8">
-    <title>Select</title>
+    <title>Plan for Smart Services(PS2)</title>
     <link rel="stylesheet" type="text/css" href="../css/styles.css"/>
+    <script>
+      function search() {
+        var search = document.getElementById("search");
+        if (search.style.display=="none") {
+          search.style.display="block";
+        }
+        else{
+          search.style.display="none";
+        }
+      }
+    </script>
   </head>
 
   <body>
-        <ul id = "menu">
-      <li><a href="index.php">Home</a></li>
+    <ul id = "menu">
+      <li><a href="../index.php">Home</a></li>
       <li><a href="#">db Maintain</a>
         <ul>
-          <li><a href="templates/insert.php">Insert</a></li>
-          <li><a href="templates/delete.php">Delete</a></li>
-          <li><a href="templates/select.php">Select</a></li>
-          <li><a href="templates/update.php">Update</a></li>
+          <li><a href="insert.php">Insert</a></li>
+          <li><a href="delete.php">Delete</a></li>
+          <li><a href="select.php">Select</a></li>
+          <li><a href="update.php">Update</a></li>
         </ul>
       </li>
-      <li><a href="templates/aboutus.php">About Us</a></li>
-      <li><a href="templates/contactus.php">Contact Us</a></li>
+      <li><a href="aboutus.php">About Us</a></li>
+      <li><a href="contactus.php">Contact Us</a></li>
+
       <?php if (isset($_SESSION['user'])){
-        echo "<li style='float:right'><a href='scripts/logout.php'>Sign Out</a></li>";
+        echo "<li style='float:right'><a href='../scripts/logout.php'>Sign Out</a></li>";
         echo "<li style='float:right'><a href=''>Welcome ". $_SESSION['user'] ."</a></li>";
+        echo '<li style="float:right"><a href="#"><span onclick="search()">Search</span></a></li>';
       }
       else {
-        echo "<li style='float:right'><a href='templates/signup.php'>Sign Up</a></li>";
+        echo "<li style='float:right'><a href='signup.php'>Sign Up</a></li>";
       }
       ?>
 
-      <li><a href="templates/reviews.php">Reviews</a></li>
+      <li><a href="reviews.php">Reviews</a></li>
       <?php if (!isset($_SESSION['user'])){
-        echo "<li style='float:right'><a href='templates/login.php'>Login</a></li>";
+        echo "<li style='float:right'><a href='login.php'>Login</a></li>";
       }
       ?>
-      <li style="float:right"><a href="#"><span onclick="search()">Search</span></a></li>
+      <!-- <li style="float:right"><a href="#"><span onclick="search()">Search</span></a></li> -->
       <li><a href="#">Type of Services</a>
         <ul>
-          <li><a href="templates/rideshare.php">Rideshare</a></li>
-          <li><a href="templates/ride_and_delivery.php">Ride & Delivery</a></li>
+          <li><a href="rideshare.php">Rideshare</a></li>
+          <li><a href="ride_and_delivery.php">Ride & Delivery</a></li>
         </ul>
       </li>
+
     </ul>
 
     <div id="search" style="float:right; display:none;">
@@ -49,7 +68,27 @@
         <button name="submit_result" type="submit">submit</button>
       </form>
     </div>
-    
+
+    <div style="text-align:right;">
+    <?php
+    if (isset($_POST['submit_result'])){
+      $search = $_POST["search"];
+      $userID = $_SESSION['userID'];
+      $sql ="SELECT * FROM order_table WHERE order_ID='$search' AND userID='$userID'";
+      //select order_id from order_table where userid = $userd, order_id = $order_id
+      $result = $dbc->query($sql);
+      $info = " ";
+      if ($result->num_rows > 0) {
+        // output data of each row
+        while($row = $result->fetch_assoc()) {
+          echo "Your order ID is ".$search." has completed.";
+        }
+      } else {
+        echo "You don't have an order with the ID ".$search.", please try again.";
+      }
+    }
+    ?>
+  </div>
     <h2>Select</h2>
     <p style="width:auto;">Please choose a table to select data:</p>
 
