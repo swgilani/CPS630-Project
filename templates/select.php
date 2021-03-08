d151d17919c70f44d0af0acde27eba4489d04f9f
Ke Zhang <57934117+keke980418@users.noreply.github.com>
2021-03-08 18:28:55 -0500

    Update select.php

--- templates/select.php
+++ templates/select.php
@@ -8,6 +8,48 @@
   </head>
 
   <body>
+        <ul id = "menu">
+      <li><a href="index.php">Home</a></li>
+      <li><a href="#">db Maintain</a>
+        <ul>
+          <li><a href="templates/insert.php">Insert</a></li>
+          <li><a href="templates/delete.php">Delete</a></li>
+          <li><a href="templates/select.php">Select</a></li>
+          <li><a href="templates/update.php">Update</a></li>
+        </ul>
+      </li>
+      <li><a href="templates/aboutus.php">About Us</a></li>
+      <li><a href="templates/contactus.php">Contact Us</a></li>
+      <?php if (isset($_SESSION['user'])){
+        echo "<li style='float:right'><a href='scripts/logout.php'>Sign Out</a></li>";
+        echo "<li style='float:right'><a href=''>Welcome ". $_SESSION['user'] ."</a></li>";
+      }
+      else {
+        echo "<li style='float:right'><a href='templates/signup.php'>Sign Up</a></li>";
+      }
+      ?>
+
+      <li><a href="templates/reviews.php">Reviews</a></li>
+      <?php if (!isset($_SESSION['user'])){
+        echo "<li style='float:right'><a href='templates/login.php'>Login</a></li>";
+      }
+      ?>
+      <li style="float:right"><a href="#"><span onclick="search()">Search</span></a></li>
+      <li><a href="#">Type of Services</a>
+        <ul>
+          <li><a href="templates/rideshare.php">Rideshare</a></li>
+          <li><a href="templates/ride_and_delivery.php">Ride & Delivery</a></li>
+        </ul>
+      </li>
+    </ul>
+
+    <div id="search" style="float:right; display:none;">
+      <form action="#" method="POST">
+        <input type="text" placeholder="Search..." name="search">
+        <button name="submit_result" type="submit">submit</button>
+      </form>
+    </div>
+    
     <h2>Select</h2>
     <p style="width:auto;">Please choose a table to select data:</p>
 
