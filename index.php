005e50b9c02be871e4786b71e6b710c986439cbd
Ke Zhang <57934117+keke980418@users.noreply.github.com>
2021-03-08 00:08:31 -0500

    Update and rename index.html to index.php

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <!DOCTYPE html>
+<?php
+require_once('db/dbc.php'); ?>
 <html>
   <head>
     <meta charset="utf-8">
@@ -20,7 +22,14 @@
   <body>
     <ul id = "menu">
       <li><a href="index.html">Home</a></li>
-      <li><a href="#">System Logo</a></li>
+      <li><a href="#">db Maintain</a>
+        <ul>
+          <li><a href="#">Insert</a></li>
+          <li><a href="#">Delete</a></li>
+          <li><a href="#">Select</a></li>
+          <li><a href="#">Update</a></li>
+        </ul>
+      </li>
       <li><a href="templates/aboutus.html">About Us</a></li>
       <li><a href="templates/contactus.html">Contact Us</a></li>
       <li style="float:right"><a href="templates/signup.html">Sign up</a></li>
@@ -34,16 +43,36 @@
       </li>
       <li style="float:right"><a href="#"><span onclick="search()">Search</span></a></li>
     </ul>
-    
+
     <div id="search" style="float:right; display:none;">
-      <form action="scripts/search_order.php">
+      <form action="#" method="POST">
         <input type="text" placeholder="Search..." name="search">
-        <button type="submit">submit</button>
+        <button name="submit_result" type="submit">submit</button>
       </form>
     </div>
-    
+
     <br><br>
     <h1>Plan for Smart Services</h1>
-    <img src="img/logo.png" class="center">  
+    <img src="img/logo.png" class="center">
+
+    <div style="text-align:right;">
+    <?php
+    if (isset($_POST['submit_result'])){
+      $search = $_POST["search"];
+      $sql = "SELECT * FROM order_table WHERE order_ID =" . $search;
+      //select order_id from order_table where userid = $userd, order_id = $order_id
+      $result = $dbc->query($sql);
+      $info = " ";
+      if ($result->num_rows > 0) {
+        // output data of each row
+        while($row = $result->fetch_assoc()) {
+          echo "Your order ID is ".$search." has completed.";
+        }
+      } else {
+        echo "Cannot find order ID ".$search.", please try again.";
+      }
+    }
+    ?>
+  </div>
   </body>
 </html>
